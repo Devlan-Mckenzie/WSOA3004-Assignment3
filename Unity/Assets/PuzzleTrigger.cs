d280925c3a84f8f705c3a0d282024aa17f4e5fd6
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-08-25 15:32:41 +0200

    Made a puzzle
    Basic puzzle and tweaked some comp settings to fix colliders (vertex distance)

---
+++ Unity/Assets/PuzzleTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTrigger : MonoBehaviour
+{
+    [Header("Door Reference")]
+    public GameObject Door_1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PuzzlePiece")
+        {
+            Door_1.SetActive(false);
+        }
+    }
+}
