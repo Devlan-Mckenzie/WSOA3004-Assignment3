274a072f0da50352124dd1e06f7582e78868d2d8
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-08-25 17:42:41 +0200

    Enemy Collision and Puzzle Timer
    Created 2 scripts for level reset and puzzle timer before spawning enemy.

---
+++ Unity/Assets/Scripts/PuzzleTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTimer : MonoBehaviour
+{
+    public float TimeGiven = 10;
+
+    private float TimerCount = 0;
+    [SerializeField] GameObject EnemyPrefab;
+    [SerializeField] bool StartTimer = false;
+    [SerializeField] bool EnemySpawned = false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !EnemySpawned)
+        {
+            StartTimer = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (StartTimer)
+        {
+            TimerCount += Time.deltaTime;         
+        }
+
+        if (TimerCount > TimeGiven && StartTimer)
+        {
+            StartTimer = false;            
+            Instantiate(EnemyPrefab);
+            EnemySpawned = true;           
+        }
+    }
+}
