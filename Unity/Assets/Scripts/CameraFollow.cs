3dfc6e781bddcf03c535fe75c1d545574adee867
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-08-27 14:37:27 +0200

    Created Camera Follow and Built Game
    Created a Camera Follow script so unparneted the main camera and built the game. In terms of play testing you can find the build in a folder called builds and then a sub folder called First build pre alpha

---
+++ Unity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosistion = Player.transform.position + offset;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPosistion, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPostion;        
+    }
+}
