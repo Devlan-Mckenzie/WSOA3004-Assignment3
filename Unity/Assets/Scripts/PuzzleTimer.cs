d3dda4d79b7b88858f985e708e7b9d074967ae9c
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-09-03 13:21:02 +0200

    Created Visual Puzzle Timer
    In the TileMap scene and Updated to be part of the PuzzleTimer script.

--- Unity/Assets/Scripts/PuzzleTimer.cs
+++ Unity/Assets/Scripts/PuzzleTimer.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PuzzleTimer : MonoBehaviour
 {
+    public Slider TimerSlider;
+    public GameObject TextStorage;
+    public TextMeshProUGUI  TimerText;
     public float TimeGiven = 10;
 
     private float TimerCount = 0;
@@ -11,6 +16,18 @@ public class PuzzleTimer : MonoBehaviour
     [SerializeField] bool StartTimer = false;
     [SerializeField] bool EnemySpawned = false;
     [SerializeField] bool PuzzleCompleted = false;
+    float SliderTime;
+
+    private void Start()
+    {
+        StartTimer = false;
+        TimerSlider.maxValue = TimeGiven;
+        TimerSlider.value = TimeGiven;
+
+        TimerText = TextStorage.GetComponent<TextMeshProUGUI>();
+
+        SliderTime = TimeGiven;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && !EnemySpawned)
@@ -21,10 +38,23 @@ public class PuzzleTimer : MonoBehaviour
 
     private void Update()
     {
+
         if (StartTimer)
-        {
-            TimerCount += Time.deltaTime;         
+        {   SliderTime -= Time.deltaTime;
+            float SliderMinutes = Mathf.FloorToInt(SliderTime / 60);
+            float SliderSeconds =  Mathf.FloorToInt(SliderTime - SliderMinutes *60f);    
+            
+            string textTime = string.Format("{0:0}:{1:00}", SliderMinutes, SliderSeconds);
+
+            if (SliderTime <= 0)
+            {
+                StartTimer = false;
+            }          
+            
+            TimerText.text = textTime;
+            TimerSlider.value = SliderTime;            
         }
+        
 
         if (!PuzzleCompleted)
         {
