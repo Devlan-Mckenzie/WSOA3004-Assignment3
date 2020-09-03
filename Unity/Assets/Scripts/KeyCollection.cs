590142fe9e8ad35bc1310519db824d310271c43a
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-09-03 15:28:53 +0200

    Fixed Key Script Crash error
    Reason for error and crash unknown, fixed now after redoing it

--- Unity/Assets/Scripts/KeyCollection.cs
+++ Unity/Assets/Scripts/KeyCollection.cs
@@ -7,58 +7,59 @@ public class KeyCollection : MonoBehaviour
     public GameObject Key_Symbol_1;
     public GameObject Key_Symbol_2;
     public GameObject Key_Symbol_3;
+
     private int keyCount = 0;
     public bool pickedupkey = false;
 
     private void Update()
     {
-        while (keyCount > 0)
+        if (keyCount > 0)
         {
             pickedupkey = true;
-        }
-        if (keyCount < 0)
+        } else
         {
             pickedupkey = false;
         }
     }
 
-    public void RemoveKey()
-    {
-        keyCount--;
-    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Key")
         {
-            ++keyCount;
-            //FindObjectOfType<AudioManager>().Play("KeyCollectionSound");
-            //Destroy(collision.gameObject);
+            keyCount++;
+            FindObjectOfType<AudioManager>().Play("KeyCollectionSound");
+            Destroy(collision.gameObject);
             if (keyCount >= 1)
             {
-                Key_Symbol_1.SetActive(true);                               
-            }
-            else
+                Key_Symbol_1.SetActive(true);
+            }else
             {
                 Key_Symbol_1.SetActive(false);
             }
+
             if (keyCount >= 2)
             {
-                Key_Symbol_2.SetActive(true);                              
+                Key_Symbol_2.SetActive(true);
             }
             else
             {
                 Key_Symbol_2.SetActive(false);
             }
+
             if (keyCount >= 3)
             {
-                Key_Symbol_3.SetActive(true);                
+                Key_Symbol_3.SetActive(true);
             }
             else
             {
                 Key_Symbol_3.SetActive(false);
             }
-
         }
+    }
+    public void RemoveKey()
+    {
+        keyCount--;
     }
 
 }
