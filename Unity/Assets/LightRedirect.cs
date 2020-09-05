140983129920761afa910a5ba1166642957f1b6a
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-09-05 12:36:10 +0200

    Light Redirection implemented

---
+++ Unity/Assets/LightRedirect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRedirect : MonoBehaviour
+{
+    public GameObject Light;
+
+    [SerializeField]
+    private float rayLength = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Light.activeSelf)      
+        {           
+
+            //get direction vector from light to upwards position in world space
+            Vector3 direction = transform.up;            
+
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction, rayLength);
+
+            if (hit.collider != null && hit.collider.CompareTag("LightTrigger"))
+            {
+                hit.collider.GetComponent<LightTrigger>().OpenDoor();
+            }
+
+            //Debug.DrawLine(this.transform.position, hit.point, Color.red);
+        }
+    }
+
+    public void LightOn()
+    {
+        Light.SetActive(true);
+    }
+}
---
+++ Unity/Assets/Scripts/LightTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour
+{
+    public GameObject Door;
+    public void OpenDoor()
+    {
+        Door.SetActive(false);
+    }
+}
