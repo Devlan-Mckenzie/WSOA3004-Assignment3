8fc40feb993335bcf610a6ad7fd90002305cd127
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-09-11 17:26:12 +0200

    Win Screen Done

--- Unity/Assets/Scripts/FinalDoor.cs
+++ Unity/Assets/Scripts/FinalDoor.cs
@@ -6,12 +6,15 @@ public class FinalDoor : MonoBehaviour
 {
     public int finalKeycount = 0;    // Stores the final key count
     public int RequiredFinalKeys = 3;
+    public GameObject WinScreen;    // Stores a refrence to the win screen for displaying the win at the end of the game YAY !!!
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // if the player collides with this door and has 3 final keys 
         if(finalKeycount == RequiredFinalKeys && collision.gameObject.tag== "Player")
         {
+            // Enable to win screen
+            WinScreen.SetActive(true);
             // destroy this door
             Destroy(this.gameObject);
         }
