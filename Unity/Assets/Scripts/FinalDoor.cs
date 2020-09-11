753dd746a718fcd2ae53f7008a84ed6845501565
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-09-11 15:41:55 +0200

    Froze the Rotation on the Z for the boxes

--- Unity/Assets/Scripts/FinalDoor.cs
+++ Unity/Assets/Scripts/FinalDoor.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class FinalDoor : MonoBehaviour
 {
     public int finalKeycount = 0;    // Stores the final key count
+    public int RequiredFinalKeys = 3;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // if the player collides with this door and has 3 final keys 
-        if(finalKeycount == 3 && collision.gameObject.tag== "Player")
+        if(finalKeycount == RequiredFinalKeys && collision.gameObject.tag== "Player")
         {
             // destroy this door
             Destroy(this.gameObject);
