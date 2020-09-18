79c94cf3c6f090040b28abc8d29206669031883d
Ben-EL <1860303@students.wits.ac.za>
2020-09-18 13:25:57 +0200

    Added Camera Zoom
    Added camera zoom, can control it using mouse wheel or comment out its initialization in the start function. Changed camera follow y offset to -8, if you want to change it back.

---
+++ Unity/Assets/Scripts/CameraZoomController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomController : MonoBehaviour
+{
+    public Camera cam;
+    private float targetZoom;
+    public float zoomFactor;
+    public float minZoom;
+    public float maxZoom;
+    public float zoomLerpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+
+        // Target zoom can be set to start 
+         targetZoom = maxZoom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float getAxis;
+        getAxis = Input.GetAxis("Mouse ScrollWheel"); // use mouse wheel to zoom in and out
+
+        targetZoom -= getAxis * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom); // clamped so that image is not inverted
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+    }
+}
