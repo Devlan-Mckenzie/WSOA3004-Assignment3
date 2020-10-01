cec93f9019960b040377d7abd0227491ad2452b9
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-10-01 12:56:34 +0200

    Basic Movement Done
    Vertical and Horizontal movement with diagonal capabilities

---
+++ Unity/Assets/CharacterControllerBeta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerBeta : MonoBehaviour
+{
+    [SerializeField]
+    private float maxSpeed = 10f;
+
+    private new Rigidbody2D rigidbody2D;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Read all the inputs 
+        float v = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Vertical");
+        Move(h, v);
+    }
+
+    void Move(float HorizontalMove, float VerticalMove)
+    {
+        // Move the character in the horizontal axis
+        rigidbody2D.velocity = new Vector2(HorizontalMove * maxSpeed, rigidbody2D.velocity.y);
+
+        //Move the Character in the Vertical Axis
+        rigidbody2D.velocity = new Vector2(VerticalMove * maxSpeed, rigidbody2D.velocity.x);
+    }
+}
