3901858a77e7476254ceb05d6baf001c327159de
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-10-01 16:34:53 +0200

    Health and Stam Bar in and working
    Player Death and hurt animation tested.
    Implemented a stam and health bar.

--- Unity/Assets/PlayerCombat.cs
+++ Unity/Assets/PlayerCombat.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     public int attackDamage = 40;
+    public int attackStamina = 20;
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
@@ -14,9 +15,26 @@ public class PlayerCombat : MonoBehaviour
     public float attackRate = 2f;
     private float nextAttackTime = 0f;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    public int maxStamina = 100;
+    public int currentStamina;
+
+    public StaminaBar staminaBar;
+
+
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        currentStamina = maxStamina;
+        staminaBar.SetMaxStamina(maxStamina);
     }
 
     // Update is called once per frame
@@ -24,7 +42,7 @@ public class PlayerCombat : MonoBehaviour
     {
         if (Time.time >= nextAttackTime)
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0) && currentStamina >= attackStamina)
             {
                 Attack();
                 nextAttackTime = Time.time + 1f / attackRate;
@@ -35,6 +53,9 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
+        currentStamina -= attackStamina;
+        staminaBar.SetStamina(currentStamina);
+
         //Play attack animation
         animator.SetTrigger("Attack");
 
@@ -55,5 +76,24 @@ public class PlayerCombat : MonoBehaviour
             return;
         }
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    public void PlayerTakeDamage(int damage)
+    {
+        animator.SetTrigger("Hurt");
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<CharacterControllerBeta>().enabled = false;
+        this.enabled = false;
     }
 }
---
+++ Unity/Assets/StaminaBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxStamina(int Stamina)
+    {
+        slider.maxValue = Stamina;
+        slider.value = Stamina;
+    }
+
+    public void SetStamina(int stamina)
+    {
+        slider.value = stamina;
+    }
+}
