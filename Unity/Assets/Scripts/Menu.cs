4f2d368e0bbdef6a0dab1b3d8b03430fcc3d8ef7
ChristopherMopp <64950889+ChristopherMopp@users.noreply.github.com>
2020-10-05 11:11:12 +0200

    added menu scripts

---
+++ Unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void Closegame()
+    {
+        Application.Quit();
+    }
+
+    public void next2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void rules()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void next()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
