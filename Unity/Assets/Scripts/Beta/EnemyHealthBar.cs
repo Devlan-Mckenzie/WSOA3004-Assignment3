7584c3266c399227d9fc784832ebd84bbb8780be
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-10-08 11:21:11 +0200

    Fixed the audio manager duplication stuff and enemy hp bar bug

--- Unity/Assets/Scripts/Beta/EnemyHealthBar.cs
+++ Unity/Assets/Scripts/Beta/EnemyHealthBar.cs
@@ -18,6 +18,7 @@ public class EnemyHealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        EnemyHealth.value = FindObjectOfType<Enemy>().currentHealth;
+        EnemyHealth.value = GetComponentInParent<Enemy>().currentHealth; 
+        
     }
 }
