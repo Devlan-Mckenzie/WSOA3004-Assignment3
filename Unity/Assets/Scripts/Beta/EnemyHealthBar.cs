b3babb45d0575f5f3930e938e80a7689b0a79e20
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-10-09 12:00:08 +0200

    Shadows Implemented

--- Unity/Assets/Scripts/Beta/EnemyHealthBar.cs
+++ Unity/Assets/Scripts/Beta/EnemyHealthBar.cs
@@ -6,19 +6,28 @@ using UnityEngine.UI;
 public class EnemyHealthBar : MonoBehaviour
 {
     public Slider EnemyHealth;
+    public GameObject ShadowAlive;
+    public GameObject ShadowDead;
 
     public int health;
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
-        EnemyHealth = this.gameObject.GetComponent<Slider>();
+        EnemyHealth = this.gameObject.GetComponent<Slider>();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemyHealth.value = GetComponentInParent<Enemy>().currentHealth; 
+        EnemyHealth.value = GetComponentInParent<Enemy>().currentHealth;
+
+        if (EnemyHealth.value <= 0)
+        {
+            EnemyHealth.gameObject.SetActive(false);
+            ShadowAlive.SetActive(false);
+            ShadowDead.SetActive(true);           
+        }
         
     }
 }
