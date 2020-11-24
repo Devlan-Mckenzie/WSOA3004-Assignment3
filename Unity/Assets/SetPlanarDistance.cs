9dcae57d5e822fe6e340ac58fd787effa4ad341f
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-11-24 11:54:34 +0200

    Code to alter planar distance

---
+++ Unity/Assets/SetPlanarDistance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlanarDistance : MonoBehaviour
+{
+    private bool PlanarSet = false;
+    public float NewPlanarDistance = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<Canvas>().worldCamera != null && !PlanarSet)
+        {
+            this.GetComponent<Canvas>().planeDistance = NewPlanarDistance;
+            PlanarSet = true;
+        }
+    }
+}
