493981702dd4006ba9d9be4e020cecc3c306c38d
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-11-27 12:27:41 +0200

    Update CanvasController.cs

--- Unity/Assets/Scripts/Beta/CanvasController.cs
+++ Unity/Assets/Scripts/Beta/CanvasController.cs
@@ -13,6 +13,5 @@ public class CanvasController : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
-        Debug.Log("canvas flip");
     }
 }
