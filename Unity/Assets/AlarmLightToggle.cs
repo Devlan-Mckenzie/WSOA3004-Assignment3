6df955b47fe86fd71992d254ea1363c44b3163cd
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-11-27 11:54:37 +0200

    Made Alarm Sparkly (not really)

---
+++ Unity/Assets/AlarmLightToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class AlarmLightToggle : MonoBehaviour
+{
+    public GameObject[] AlarmLights;
+
+    public void ToggleGreen()
+    {
+        Debug.Log("Green");
+        for (int i = 0; i < AlarmLights.Length; i++)
+        {
+            AlarmLights[i].GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().color = Color.green;
+        }
+    }
+
+    public void ToggleRed()
+    {
+        Debug.Log("Red");
+        for (int i = 0; i < AlarmLights.Length; i++)
+        {
+            AlarmLights[i].GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().color = Color.red;
+        }
+    }
+}
