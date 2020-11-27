8328e5166c587b261abfd2a03f8641d6a15b0b79
ChristopherMopp <64950889+ChristopherMopp@users.noreply.github.com>
2020-11-27 09:29:48 +0200

    trying to fix text in PR2

--- Unity/Assets/Scripts/Beta/CanvasController.cs
+++ Unity/Assets/Scripts/Beta/CanvasController.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
+    public Text Prisoner2Text;
+
+    
     public void CanvasFlip()
     {
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
-        transform.localScale = theScale;      
+        transform.localScale = theScale;
+
+        TextFlip();
+    }
+
+    public void TextFlip()
+    {
+        Vector3 theScale = Prisoner2Text.transform.localScale;
+        theScale.x *= -1;
+
+        Prisoner2Text.transform.localScale = theScale;
     }
 }
